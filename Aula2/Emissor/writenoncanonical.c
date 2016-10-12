d0c469884541e3111fa2ccb8ad365881863baf26
rodavoce <up201404178@fe.up.pt>
2016-10-12 01:13:23 +0100

    emissor working

--- Aula2/Emissor/writenoncanonical.c
+++ Aula2/Emissor/writenoncanonical.c
@@ -9,21 +9,37 @@
 #include <string.h>
 #include <unistd.h>
 #include "util.h"
-#include "alarme.c"
+#include <signal.h>
+
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
-#define SEND 0
-#define RECEIVE 1
+
+#define START 0
+#define FLAG_RCV 1
+#define A_RCV 2
+#define C_RCV 3
+#define BCC_RCV 4
+#define COMPLETE 5
 
 volatile int STOP=FALSE;
 
 char * createSet();
 int sendMensage(int fd, char *message, int length);
 int createAndSendSet(int fd);
+int receiveFlag(int fd);
+int receiveA(int fd, char *ch);
+int receiveC(int fd, char *ch);
+int checkBCC(int fd, char A, char C);
+int receiveUA(int fd);
+
+
+volatile int flag=1, conta=1;
+void installAlarm();
+void atende();
 
 int main(int argc, char** argv)
 {
@@ -39,7 +55,7 @@ int main(int argc, char** argv)
       exit(1);
     }
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    fd = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
     if (fd <0) {perror(argv[1]); exit(-1); }
 
     if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
@@ -65,23 +81,34 @@ int main(int argc, char** argv)
       perror("tcsetattr");
       exit(-1);
     }
-/*	int state = 0;
-	switch(state)
-{
-	case SEND:
-		createAndSendSet(fd);
-	break;
-	case RECEIVE:
-		
-	break;
-}*/
-	createAndSendSet(fd);
 	
 	
+	int noResponse = 1;
 
-	sleep(2);
 
-	//installAlarm();
+	installAlarm();
+	
+	
+ 	while(conta < 4 && noResponse != COMPLETE){
+	
+	
+	if(flag)
+		{
+			alarm(3);
+			flag=0;
+			createAndSendSet(fd);
+			
+		}
+	
+	noResponse = receiveUA(fd);
+	
+	
+	}
+	
+
+
+	
+	
 /*	gets(buf);
     
 	int length = strlen(buf);
@@ -109,14 +136,14 @@ int main(int argc, char** argv)
 
    printf("Received %d bytes: %s\n", total, buf);
 	
-
+*/
 
 
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
-	*/
+
     close(fd);
     return 0;
 }
@@ -148,4 +175,109 @@ int createAndSendSet(int fd){
 	return res;
 }
 
+int receiveUA(int fd)
+{
+	int status = 0;
+	int res;
+	char A, C;
+	do
+	{
+		switch(status)
+		{
+			case START:
+			status = receiveFlag(fd);
+			break;
+			case FLAG_RCV:
+			status = receiveA(fd, &A);
+			break;
+			case A_RCV:
+			status = receiveC(fd, &C);
+			break;
+			case C_RCV:
+			status = checkBCC(fd, A, C);
+			break;
+			case BCC_RCV:
+			res = receiveFlag(fd);
+			if(res == FLAG_RCV)
+ 			{
+				printf("BCC Checked successfully\n");
+				status = COMPLETE;
+			}
+			break;
+		}
+	}while(status != COMPLETE && flag == 0);
 
+	return status;
+}
+
+
+int receiveFlag(int fd)
+{//printf("antes FLAG \n");
+	char ch;
+	read(fd, &ch, 1);
+	
+	//printf("%x \n",ch);
+	if(ch == F_FLAG)
+		return A_RCV;
+	else
+		return FLAG_RCV;
+}
+
+int receiveA(int fd, char* ch)
+{
+	int res;
+	res = read(fd, ch, 1);
+	//printf("%x \n",*ch);
+	if(res <= 0)
+		return START;
+	else if(*ch == F_FLAG)
+		return F_FLAG;
+	return C_RCV;
+}
+
+int receiveC(int fd, char* ch)
+{
+	
+	int res;
+	res = read(fd, ch, 1);
+	printf("%x \n",*ch);
+	if(res <= 0)
+		return START;
+	else if(*ch == F_FLAG)
+		return F_FLAG;
+	return BCC_RCV;
+}
+
+int checkBCC(int fd, char A, char C)
+{
+	char ch;
+	read(fd, &ch, 1);
+	char expected = A ^ C;
+
+printf("%x \n",expected);
+	if(ch == expected)
+		return COMPLETE;
+	else
+		return -1;
+}
+
+
+
+
+void atende()                   // atende alarme
+{
+	printf("alarme # %d\n", conta);
+	flag=1;
+	conta++;
+}
+
+
+void installAlarm()
+{
+
+(void) signal(SIGALRM, atende);  // instala  rotina que atende interrupcao
+
+ 
+printf("Vou terminar.\n");
+
+}
